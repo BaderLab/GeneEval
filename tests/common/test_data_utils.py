63d59c381524ce2718c8615060337a8bc7748e83
johngiorgi <johnmgiorgi@gmail.com>
2020-07-29 14:22:13 -0400

    :bug: Fix issues breaking build

--- tests/common/test_data_utils.py
+++ tests/common/test_data_utils.py
@@ -3,37 +3,39 @@ from hypothesis.strategies import text
 from hypothesis import given
 from geneeval.common.data_utils import load_embeddings
 import pandas as pd
+from pathlib import Path
 
 
 class TestDataUtils:
     @given(filepath=text())
     def test_value_error_load_embeddings(self, filepath: str) -> None:
+        filepath = Path(filepath)
         with pytest.raises(ValueError):
             load_embeddings(filepath)
 
     def test_load_embeddings_json(
-        self, embeddings_dataframe: pd.DataFrame, embeddings_json_filepath: str
+        self, embeddings_dataframe: pd.DataFrame, embeddings_json_filepath: Path
     ) -> None:
         pd.testing.assert_frame_equal(
             embeddings_dataframe, load_embeddings(embeddings_json_filepath),
         )
 
     def test_load_embeddings_tsv(
-        self, embeddings_dataframe: pd.DataFrame, embeddings_tsv_filepath: str
+        self, embeddings_dataframe: pd.DataFrame, embeddings_tsv_filepath: Path
     ) -> None:
         pd.testing.assert_frame_equal(
             embeddings_dataframe, load_embeddings(embeddings_tsv_filepath),
         )
 
     def test_load_embeddings_csv(
-        self, embeddings_dataframe: pd.DataFrame, embeddings_csv_filepath: str
+        self, embeddings_dataframe: pd.DataFrame, embeddings_csv_filepath: Path
     ) -> None:
         pd.testing.assert_frame_equal(
             embeddings_dataframe, load_embeddings(embeddings_csv_filepath),
         )
 
     def test_load_embeddings_txt(
-        self, embeddings_dataframe: pd.DataFrame, embeddings_txt_filepath: str
+        self, embeddings_dataframe: pd.DataFrame, embeddings_txt_filepath: Path
     ) -> None:
         pd.testing.assert_frame_equal(
             embeddings_dataframe, load_embeddings(embeddings_txt_filepath),

47e88be0e3ea4386421b13fabc969e3a69f88e2c
johngiorgi <johnmgiorgi@gmail.com>
2020-08-03 14:35:36 -0400

    :books: CLI argument documentation

--- geneeval/main.py
+++ geneeval/main.py
@@ -1,6 +1,8 @@
 import typer
-from typing import Optional, List
+from typing import List, Union
 from pathlib import Path
+from geneeval import Engine
+from geneeval.common.data_utils import load_embeddings
 
 app = typer.Typer()
 
@@ -8,11 +10,11 @@ app = typer.Typer()
 @app.command()
 def prepare(
     filepath: Path = typer.Argument(..., writable=True, help="Filepath to save the task data."),
-    include_tasks: Optional[List[str]] = typer.Option(
-        None, help="A list of task names to include in the evaluation."
+    include_tasks: Union[str, List[str]] = typer.Option(
+        None, help="A task name (or list of task names) to include in the prepared data."
     ),
-    exclude_tasks: Optional[List[str]] = typer.Option(
-        None, help="A list of task names to exclude in the evaluation."
+    exclude_tasks: Union[str, List[str]] = typer.Option(
+        None, help="A task name (or list of task names) to exclude in the prepared data."
     ),
 ) -> None:
     typer.echo("Downloading data for task...")
@@ -23,12 +25,21 @@ def evaluate(
     filepath: Path = typer.Argument(
         ..., exists=True, dir_okay=False, help="Filepath to the gene embeddings."
     ),
-    include_tasks: Optional[List[str]] = typer.Option(
-        None, help="A list of task names to include in the evaluation."
+    include_tasks: Union[str, List[str]] = typer.Option(
+        None, help="A task name (or list of task names) to include in the evaluation."
     ),
-    exclude_tasks: Optional[List[str]] = typer.Option(
-        None, help="A list of task names to exclude in the evaluation."
+    exclude_tasks: Union[str, List[str]] = typer.Option(
+        None, help="A task name (or list of task names) to exclude in the evaluation."
     ),
 ) -> None:
+
+    embeddings = load_embeddings(filepath)
+    engine = Engine(embeddings, include_tasks, exclude_tasks)
+    engine.run()
+
+    # Nicely display results in the console.
+    # Save the results to disk (should be a format that is easy to load)
+    results = engine.results
+    print(results)
 
     typer.echo("Evaluating data for task...")

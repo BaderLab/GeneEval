45bb8a0651658417d90f4813de0563eb8ffda0ab
johngiorgi <johnmgiorgi@gmail.com>
2020-10-14 19:19:43 -0400

    :recycle: Make certain utils importable from root

--- geneeval/__init__.py
+++ geneeval/__init__.py
@@ -2,3 +2,4 @@ from geneeval.data import DatasetReader
 from geneeval.engine import Engine
 from geneeval.classifiers.auto_classifier import AutoClassifier
 from geneeval.metrics.auto_metric import AutoMetric
+from geneeval.common.data_utils import load_features, load_benchmark
